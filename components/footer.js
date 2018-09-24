3ff335571b30b102cc62098f806ec2a58ca7fcfe
johnstonjacob <jacob@johnstonjacob.com>
2018-09-24 14:36:09 -0500

    footer

---
+++ components/footer.js
@@ -0,0 +1,12 @@
+import "../styles/stylesheet.css"
+
+const Footer = () => (
+    <ul className="pure-menu-list footer">
+        <li className="pure-menu-item"><a href="https://github.com/johnstonjacob" className="pure-menu-link">Github</a></li>
+        <li className="pure-menu-item"><a href="https://linkedin.com/in/johnstonajacob" className="pure-menu-link">Linkedin</a></li>
+        <li className="pure-menu-item"><a href="mailto:jacob@johnstonjacob.com" className="pure-menu-link">Email</a></li>
+        <li className="pure-menu-item"><a href="//TODO" className="pure-menu-link">CV</a></li>
+    </ul>
+)
+
+export default Footer;
