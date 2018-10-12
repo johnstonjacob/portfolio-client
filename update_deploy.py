ea8ecd0d57c157ac9086aa0669d7e9fff748057d
johnstonjacob <jacob@johnstonjacob.com>
2018-10-12 14:22:55 -0500

    update deploy

---
+++ update_deploy.py
@@ -0,0 +1,58 @@
+#! /usr/local/bin/python3
+import boto3
+import sys 
+import os
+
+def execute_commands_on_linux_instances(client, commands, instance_ids):
+    region = os.environ["S3_REGION"]
+    output_bucket = os.environ["OUTPUT_BUCKET"]
+    role_arn = os.environ["ROLE_ARN"]
+    notification_arn = os.environ["NOTIFICATION_ARN"]
+    
+    resp = client.send_command(
+        DocumentName="AWS-RunShellScript", # One of AWS' preconfigured documents
+        Parameters={
+            'commands': commands,
+            'workingDirectory': ['~']
+        },
+        OutputS3Region=region,
+        OutputS3BucketName=output_bucket,
+        ServiceRoleArn=role_arn,
+        NotificationConfig= {
+            'NotificationArn': notification_arn,
+            'NotificationEvents': ["Failed", "TimedOut", "Cancelled"],
+            'NotificationType': "Command"
+        },
+        InstanceIds=instance_ids,
+    )
+    return resp
+
+def getTaggedInstances(client, value):
+    instance_ids = []
+    for instance in client.instances.all():
+        for tag in instance.tags:
+            if tag["Key"] == "aws:cloudformation:stack-name":
+                if tag["Value"] == value:
+                    instance_ids.append(instance.id)
+    return instance_ids
+
+
+if len(sys.argv) < 3:
+    print("Insuffecient parameters, requires tag and script.")
+    sys.exit(1)
+
+tag = sys.argv[1]
+script_location = sys.argv[2]
+
+bucket, path = script_location.split("/", 1)
+
+ssm_client = boto3.client('ssm') 
+ec2_resource = boto3.resource("ec2")
+s3_client = boto3.client("s3")
+
+script = s3_client.get_object(Bucket=bucket, Key=path)["Body"].read().decode("utf8")
+
+commands = [script]
+instance_ids = getTaggedInstances(ec2_resource, tag)
+
+execute_commands_on_linux_instances(ssm_client, commands, instance_ids)
